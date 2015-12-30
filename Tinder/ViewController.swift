eda721af95eeeaf1df3be9a8d55024115d653b8f
Ivan Magda <imagda15@gmail.com>
2015-12-30 19:23:17 +0900

    Initial Commit

---
+++ Tinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tinder
+//
+//  Created by Иван Магда on 30.12.15.
+//  Copyright © 2015 Ivan Magda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
